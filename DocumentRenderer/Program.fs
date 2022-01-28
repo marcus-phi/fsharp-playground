781d649b6e28536bf100d6ae6e6f06462b45b053
Marcus Phi <marcus@marcusphi.com>
2022-01-28 00:19:22 -0600

    draw document from flat structure

--- DocumentRenderer/Program.fs
+++ DocumentRenderer/Program.fs
@@ -1,4 +1,5 @@
 ﻿open SkiaSharp
+open System.IO
 
 type Rect =
     { Left: float32
@@ -12,6 +13,47 @@ type ScreenElement =
     | TextElement of TextContent * Rect
     | ImageElement of string * Rect
 
+let deflate rc dw dh =
+    { Left = rc.Left + dw
+      Top = rc.Top + dh
+      Width = rc.Width - dw * 2f
+      Height = rc.Height - dh * 2f }
+
+let toSKRect rc =
+    SKRect(rc.Left, rc.Top, rc.Left + rc.Width, rc.Top + rc.Height)
+
+let drawString (str: string) x y font brush (canvas: SKCanvas) =
+    str.Split "\n"
+    |> Seq.fold
+        (fun y line ->
+            let blob = SKTextBlob.Create(line, font)
+            canvas.DrawText(blob, x, y + blob.Bounds.Height, brush)
+            y + blob.Bounds.Height)
+        y
+    |> ignore
+
+let drawElements elements (canvas: SKCanvas) =
+    use brush = new SKPaint(Color = SKColors.Black)
+
+    elements
+    |> List.map (fun e ->
+        match e with
+        | TextElement (tc, rc) -> drawString tc.Text rc.Left rc.Top tc.Font brush canvas
+        | ImageElement (filename, rc) ->
+            let dw, dh = rc.Width / 10f, rc.Height / 10f
+            let rc = toSKRect (deflate rc dw dh)
+            use bmp = SKBitmap.Decode(filename)
+            canvas.DrawBitmap(bmp, rc))
+
+    |> ignore
+
+let drawImage (w, h) space f =
+    use surface = SKSurface.Create(SKImageInfo(w, h))
+    let canvas = surface.Canvas
+    canvas.Clear SKColors.White
+    canvas.Translate(space, space)
+    f canvas
+    surface.Snapshot()
 
 let main =
     use font = SKTypeface.FromFamilyName "Arial"
@@ -36,11 +78,12 @@ let main =
           )
           TextElement(
               { Text =
-                  @"In this book, we'll introduce you to the essential
-                    concepts of functional programming, but thanks to the .NET
-                    framework, we won't be limited to theoretical examples and we
-                    will use many of the rich .NET libraries to show how functional
-                    programming can be used in a real-world."
+                  [ "In this book, we'll introduce you to the essential"
+                    "concepts of functional programming, but thanks to the .NET"
+                    "framework, we won't be limited to theoretical examples and we"
+                    "will use many of the rich .NET libraries to show how functional"
+                    "programming can be used in a real-world." ]
+                  |> String.concat "\n"
                 Font = textFont },
               { Left = 10f
                 Top = 230f
@@ -48,6 +91,9 @@ let main =
                 Height = 400f }
           ) ]
 
-    ignore
+    let image = drawImage (400, 450) 20f (drawElements elements)
+    let png = image.Encode()
+    use file = File.OpenWrite "output.png"
+    png.SaveTo file
 
-main ()
+main
