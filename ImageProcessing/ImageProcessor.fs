383f8577f87b60f846d213148f6dbcceab1c0497
Marcus Phi <marcus@marcusphi.com>
2022-03-12 00:38:22 -0600

    display filter list

--- ImageProcessing/ImageProcessor.fs
+++ ImageProcessing/ImageProcessor.fs
@@ -1,6 +1,7 @@
 namespace ImageProcessing
 
 open Avalonia.Controls
+open Avalonia.FuncUI.Components
 open Avalonia.FuncUI.DSL
 open Avalonia.FuncUI.Types
 open Avalonia.Layout
@@ -9,15 +10,27 @@ open Avalonia.Media.Imaging
 
 module ImageProcessor =
 
-    type Message = FileSelected of Bitmap
+    type Filter =
+        | None
+        | BlackWhite
+        | Blur
 
-    type State = { SelectedImage: Bitmap }
+    type Message =
+        | FileSelected of Bitmap
+        | FilterSelected of Filter
 
-    let init () = { SelectedImage = null }
+    type State =
+        { SelectedImage: Bitmap
+          SelectedFilter: Filter }
+
+    let init () =
+        { SelectedImage = null
+          SelectedFilter = None }
 
     let update msg state =
         match msg with
         | FileSelected file -> { state with SelectedImage = file }
+        | FilterSelected filter -> { state with SelectedFilter = filter }
 
     let view host (state: State) dispatch =
 
@@ -37,8 +50,7 @@ module ImageProcessor =
 
         let selectFileBtn =
             Button.create [ Button.content "Select file..."
-                            Button.dock Dock.Top
-                            Button.horizontalAlignment HorizontalAlignment.Center
+                            Button.horizontalAlignment HorizontalAlignment.Stretch
                             Button.onClick (fun _ -> Async.Start openImageDialog) ]
 
         let emptyPrompt =
@@ -52,8 +64,31 @@ module ImageProcessor =
                            Image.verticalAlignment VerticalAlignment.Stretch
                            Image.stretch Stretch.Uniform ]
 
+        let filterList =
+            let nameMap =
+                Map [ (None, "None")
+                      (BlackWhite, "Black & White")
+                      (Blur, "Blur") ]
+
+            let itemTemplate item =
+                TextBlock.create [ TextBlock.text nameMap.[item] ]
+
+            ListBox.create [ ListBox.dataItems [ None
+                                                 BlackWhite
+                                                 Blur ]
+                             ListBox.selectedItem None
+                             ListBox.itemTemplate (DataTemplateView<Filter>.create (fun item -> itemTemplate item))
+                             ListBox.onSelectedItemChanged (fun item -> dispatch (FilterSelected(item :?> Filter))) ]
+
+        let panel =
+            StackPanel.create [ StackPanel.dock Dock.Left
+                                StackPanel.width 150
+                                StackPanel.background "#222222"
+                                StackPanel.children [ selectFileBtn
+                                                      filterList ] ]
+
         DockPanel.create [ DockPanel.lastChildFill true
-                           DockPanel.children [ selectFileBtn
+                           DockPanel.children [ panel
                                                 if isNull state.SelectedImage then
                                                     emptyPrompt :> IView
                                                 else
